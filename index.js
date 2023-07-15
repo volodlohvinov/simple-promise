20bbb4f15d39b6e1897d05cf6ae46ce4ca319c90
Volodymyr Lohvinov <vladimirpravda2@gmail.com>
2023-07-15 14:16:15 +0200

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const promise = new Promise((resolve, reject) => {
-    setTimeout(function() {
-
-    
-    resolve ("Ready")
-    }, 2000
-    )
-        
-    
-}
-)
-promise
-  .then((result) => {
-    alert (result)
-    console.log(result)
-  }
-  
-  )
+const promise = new Promise((resolve) => {
+  setTimeout(function () {
+    resolve("Ready");
+  }, 2000);
+});
+promise.then((result) => {
+  alert(result);
+  console.log(result);
+});
